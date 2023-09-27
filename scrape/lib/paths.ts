90232f6eb656deb2feec9094eab96e131bbeb920
Clay Dunston <clay.dunston@askfta.com>
2023-09-27 10:35:40 -0400

    [WIP] scrape frame data

--- scrape/lib/paths.ts
+++ scrape/lib/paths.ts
@@ -1,6 +1,9 @@
+import { IPathname, Pathname } from "pathname-ts"
 
-const PROJECT_ROOT = process.cwd()
+const _PROJECT_ROOT = process.cwd()
+const PROJECT_ROOT: IPathname = new Pathname(_PROJECT_ROOT)
 
 export const PATHS = {
     PROJECT_ROOT,
+    TMP_FOLDER: PROJECT_ROOT.join("tmp"),
 }
--- scrape/lib/scrape.ts
+++ scrape/lib/scrape.ts
@@ -3,3 +3,52 @@ import { PATHS } from "./paths"
 export const scrape = async () => {
     console.log(PATHS.PROJECT_ROOT)
 }
+
+// `https://www.streetfighter.com/6/en-us/character/${character}/frame`
+const buildUrl = (character: CharacterNameClean) => `https://www.streetfighter.com/6/character/${character}/frame`
+
+type CharacterNameClean =
+    | "aki"
+    | "rashid"
+    | "cammy"
+    | "lily"
+    | "zangief"
+    | "jp"
+    | "marisa"
+    | "manon"
+    | "deejay"
+    | "ehonda"
+    | "dhalsim"
+    | "blanka"
+    | "ken"
+    | "juri"
+    | "kimberly"
+    | "guile"
+    | "chunli"
+    | "jamie"
+    | "luke"
+    | "ryu"
+
+interface ICharacterTableRow {
+    "Move Name"
+    // Frame
+    "Start-up"
+    "Active"
+    "Recovery"
+    // // Recovery
+    // "Hit"
+    // "Block"
+    //
+    "Cancel"
+    "Damage"
+    "Combo Scaling"
+    // Drive Gauge Increase
+    "Hit"
+    // Drive Gauge Decrease
+    "Block"
+    "Punish Counter"
+    //
+    "Super Art Gauge Increase"
+    "Properties"
+    "Miscellaneous"
+}
