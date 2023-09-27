bff7793324b4f5c1901bd9798f6f26399b7ad918
Clay Dunston <clay.dunston@askfta.com>
2023-09-27 12:12:41 -0400

    Misc ui stuff

--- src/theme/variables/colors.ts
+++ src/theme/variables/colors.ts
@@ -4,4 +4,8 @@ export const AppColors = {
     linkBlue:     "#0079d3",
     tableRowEven: "#111111",
     tableRowOdd:  "#333333",
+    // calculator
+    darkLime:   "#002200",
+    lightLime:  "#333300",
+    brightLime: "#999900",
 }
