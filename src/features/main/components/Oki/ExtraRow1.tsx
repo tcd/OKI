b73ea75a953935e79106f96c1dcc003d560b55b4
Clay Dunston <claydunston@gmail.com>
2023-09-27 17:32:02 -0500

    [WIP] Oki

---
+++ src/features/main/components/Oki/ExtraRow1.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material"
+
+export const ExtraRow1 = (): JSX.Element => {
+    return (
+        <Box>
+            <span>0........1.........2.........3.........4.........5.........6.........7.........8.........9.........10........11........</span>
+        </Box>
+    )
+}
---
+++ src/features/main/components/Oki/ExtraRow2.tsx
@@ -0,0 +1,9 @@
+import { Box } from "@mui/material"
+
+export const ExtraRow2 = (): JSX.Element => {
+    return (
+        <Box>
+            <span>12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789</span>
+        </Box>
+    )
+}
