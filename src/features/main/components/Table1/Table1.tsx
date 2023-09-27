ea7aebda36a44ad06a2f7a94bcd83b376b124c76
Clay Dunston <clay.dunston@askfta.com>
2023-09-27 15:32:42 -0400

    [WIP] add tables

--- src/features/main/components/Table1/Table1.tsx
+++ src/features/main/components/Table1/Table1.tsx
@@ -4,14 +4,50 @@ import {
 } from "@mui/material"
 
 import { Selectors } from "@app/state"
+import type { ISxProps as SxProps } from "@app/theme"
 
 export const Table1 = (): JSX.Element => {
 
     const character1 = useSelector(Selectors.Main.character1)
+    const frameData = useSelector(Selectors.Main.frameData.character1)
+
+    console.log(frameData)
+
+    const columns = Object.keys(frameData[0])
+
+    const $headers = columns.map((col, i) => (
+        <td key={i}>{col}</td>
+    ))
+
+    const $rows = frameData.map((row, i) => (
+        <tr key={i}>
+            {columns.map((col, j) => (
+                <td key={j}>
+                    {row[col] ?? ""}
+                </td>
+            ))}
+        </tr>
+    ))
 
     return (
-        <Box>
-            <span>{character1}</span>
+        <Box sx={rootSx}>
+            <table>
+                <thead>
+                    <tr>
+                        {$headers}
+                    </tr>
+                </thead>
+                <tbody>
+                    {$rows}
+                </tbody>
+            </table>
         </Box>
     )
+}
+
+// =============================================================================
+
+const rootSx: SxProps = {
+    display: "block",
+    p: 2,
 }
--- src/features/main/state/selectors/general.ts
+++ src/features/main/state/selectors/general.ts
@@ -1,12 +1,20 @@
 import type { RootState } from "@app/state"
+import { FRAME_DATA } from "@app/data"
 import { selectSlice } from "./select-slice"
 
 const character1 = (s: RootState) => selectSlice(s).character1
 const character2 = (s: RootState) => selectSlice(s).character2
+
+const character1FrameData = (s: RootState) => FRAME_DATA[character1(s)]
+const character2FrameData = (s: RootState) => FRAME_DATA[character2(s)]
 
 // =============================================================================
 
 export default {
     character1,
     character2,
+    frameData: {
+        character1: character1FrameData,
+        character2: character2FrameData,
+    },
 }
