244105279f501f95986b218505c9ec6829310cc5
Clay Dunston <claydunston@gmail.com>
2023-09-28 17:47:07 -0500

    [WIP] Oki component

--- src/features/main/components/Oki/ColRight/Col1.tsx
+++ src/features/main/components/Oki/ColRight/Col1.tsx
@@ -1,6 +1,7 @@
 import { Box } from "@mui/material"
 
 import type { ISxProps as SxProps } from "@app/theme"
+import { Selectors, useAppSelector } from "@app/state"
 
 export const Col1 = (): JSX.Element => {
     return (
@@ -10,16 +11,24 @@ export const Col1 = (): JSX.Element => {
             {/* <span>+6</span> */}
             {/* <span>+6</span> */}
             {/* <span>+55</span> */}
-            <span>+0</span>
-            <span>+0</span>
-            <span>+0</span>
-            <span>+0</span>
-            <span>+0</span>
-            <span>+0</span>
-            <span>+0</span>
+            <Cell row="1" />
+            <Cell row="2" />
+            <Cell row="3" />
+            <Cell row="4" />
+            <Cell row="5" />
+            <Cell row="6" />
+            <Cell row="7" />
             <span>&nbsp;</span>
             <span>&nbsp;</span>
         </Box>
+    )
+}
+
+const Cell = (props: { row: SF6.OkiRowNumber }): JSX.Element => {
+    const data = useAppSelector((s) => Selectors.Main.okiRowPlus(s, props.row))
+    const plus = data?.frameData?.hitAdvantage ?? 0
+    return (
+        <span>+{plus}</span>
     )
 }
 
--- src/features/main/components/Oki/ColRight/Col2.tsx
+++ src/features/main/components/Oki/ColRight/Col2.tsx
@@ -1,6 +1,7 @@
 import { Box } from "@mui/material"
 
 import type { ISxProps as SxProps } from "@app/theme"
+import { Selectors, useAppSelector } from "@app/state"
 
 export const Col2 = (): JSX.Element => {
     return (
@@ -10,16 +11,24 @@ export const Col2 = (): JSX.Element => {
             {/* <span>U HIT 7</span> */}
             {/* <span>U HIT 7</span> */}
             {/* <span>U HIT 56</span> */}
-            <span>U HIT 1</span>
-            <span>U HIT 1</span>
-            <span>U HIT 1</span>
-            <span>U HIT 1</span>
-            <span>U HIT 1</span>
-            <span>U HIT 1</span>
-            <span>U HIT 1</span>
+            <Cell row="1" />
+            <Cell row="2" />
+            <Cell row="3" />
+            <Cell row="4" />
+            <Cell row="5" />
+            <Cell row="6" />
+            <Cell row="7" />
             <span>&nbsp;</span>
             <span>&nbsp;</span>
         </Box>
+    )
+}
+
+const Cell = (props: { row: SF6.OkiRowNumber }): JSX.Element => {
+    const data = useAppSelector((s) => Selectors.Main.okiRowPlus(s, props.row))
+    const frame = data?.frameData?.startup ?? 1
+    return (
+        <span>U HIT {frame}</span>
     )
 }
 
