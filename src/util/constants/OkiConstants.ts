7597768a6c9632a74a10a4c19fbe28587b0f4010
Clay Dunston <claydunston@gmail.com>
2023-09-29 12:51:04 -0500

    Fix constant type definition for `ROWS_RANGE`

--- src/util/constants/OkiConstants.ts
+++ src/util/constants/OkiConstants.ts
@@ -2,7 +2,7 @@ import range from "lodash/range"
 
 const DOT = "·"
 const FRAMES_RANGE = range(1, 120)
-const ROWS_RANGE   = range(1, 7)
+const ROWS_RANGE   = range(1, 7) as unknown as SF6.OkiRowNumber[]
 
 export const OkiConstants = {
     DOT,
